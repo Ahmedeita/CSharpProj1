49052559fb0d3031aabf3dfc7c86c5b29fb10786
Ahmedeita <ahmedeita.dev@gmail.com>
2024-12-11 02:26:12 +0200

    Define Error Types

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Project2
+﻿using System.ComponentModel;
+
+namespace Project2
 {
     internal class Program
     {
+        static void Sum(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Error Types
+            #region Syntax Error (Compilation Error)
+            //console.WriteLine("Hello");
+            //IInt x;
+            #endregion
+            #region RunTime Error
+            //int x = 5;
+            //int y = 0;
+            //Console.WriteLine(x / y);
+
+            #endregion
+            #region Logical Error
+            //int A = 10;
+            //int B = 5;
+            //Sum(A , B);
+            #endregion
+            #region Warning
+            //  int X; // 4 Byte
+
+            #endregion
+
+            #endregion
+
+            #region
+
+            #endregion
         }
     }
 }
