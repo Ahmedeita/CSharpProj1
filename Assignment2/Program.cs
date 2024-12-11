7c97a2bf9bb9e64263410d4284cd547081502a25
Ahmedeita <ahmedeita.dev@gmail.com>
2024-12-11 03:10:48 +0200

    program that allows the user to enter a number then print it

---
+++ Assignment2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1-	Write a program that allows the user to enter a number then print it.
+            //int EnteredValue;
+            //Console.Write("Enter a number :  ");
+            //EnteredValue = int.Parse(Console.ReadLine()); 
+            //Console.WriteLine(EnteredValue);
+            //Console.ReadKey();
+            #endregion
+            #region 2- Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
+
+            #endregion
+
+
+        }
+    }
+}
